22aab22b4d70bad03b6a197705526aa2c892f429
Adi Levy <adidas.levy@gmail.com>
2013-12-08 00:32:07 +0200

    client Signed-off-by: Adi Levy <adidas.levy@gmail.com>

--- GitSpy_client.py
+++ GitSpy_client.py
@@ -30,7 +30,7 @@ def GatherCommitContent(repoName):
 	'''
 	repo = Repo(repoName)
 	commitList = list(repo.iter_commits())
-	author = str(commitList[0].author)
+	author = str(commitList[0].author.name)
 	date = str(commitList[0].committed_date)
 	message = str(commitList[0].message)
 	
